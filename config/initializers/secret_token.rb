3b6c2cc59416d0f2cd5dd075778aa269c9d20d89
Nilesh Panchal <neil08.panchal@gmail.com>
2014-01-21 02:26:04 +0530

    initial commit - contact_us working, TODO/check ur email

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactUsApp01::Application.config.secret_token = 'b2badc0bf4fc1373ce4868367c7330ba8deac463be3e9e6a167a73f1d1510ab876be5ace20590d7f199f25eab5291951c0a53c576c106016f408a215fab623ad'
